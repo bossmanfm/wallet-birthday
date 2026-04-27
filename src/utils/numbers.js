34ebd3451d063e46c7a0ed72c3dc1caf004b028b
bossmanfm <fortunefkey@gmail.com>
2026-04-27 16:33:59 +0000

    feat: add mapRange number utility

--- src/utils/numbers.js
+++ src/utils/numbers.js
@@ -110,3 +110,10 @@ export function variance(arr) {
 export function standardDeviation(arr) {
   return Math.sqrt(variance(arr));
 }
+
+/**
+ * Map value from one range to another
+ */
+export function mapRange(value, inMin, inMax, outMin, outMax) {
+  return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
