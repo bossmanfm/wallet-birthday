2e495ff08036ec5bf907b5118e3802098b41e96c
bossmanfm <fortunefkey@gmail.com>
2026-04-27 11:17:14 +0000

    docs: add JSDoc header to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,7 @@
+/**
+ * Application Entry Point
+ * Initializes React app with WagmiProvider and BrowserRouter
+ */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { WagmiProvider } from 'wagmi'
